aa1ce1e850ebc87dfff720f8fcfe32f698a97734
systemdPlusLinux <37053183+systemdPlusLinux@users.noreply.github.com>
2026-04-08 11:08:03 -0700

    Update and rename ExampleModClient.java to BetterItemVaults.java

---
+++ src/main/java/com/example/examplemod/BetterItemVaults.java
@@ -0,0 +1,9 @@
+package com.example.vaultlimit;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod("betteritemvaults")
+public class BetterItemVaults {
+    public BetterItemVaults(IEventBus modEventBus) {}
+}
