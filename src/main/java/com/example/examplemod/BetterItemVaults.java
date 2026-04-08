592e1979a6a2107b5451323629e81524951256ce
systemdPlusLinux <37053183+systemdPlusLinux@users.noreply.github.com>
2026-04-08 11:13:13 -0700

    Delete src/main/java/com/example/examplemod directory

--- src/main/java/com/example/examplemod/BetterItemVaults.java
+++
@@ -1,9 +0,0 @@
-package com.example.vaultlimit;
-
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
-
-@Mod("betteritemvaults")
-public class BetterItemVaults {
-    public BetterItemVaults(IEventBus modEventBus) {}
-}
